4b228a78923cacfa5c34e847507ce7af65e251f4
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-21 13:48:06 -0600

    feature: section to show calories consumed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -36,6 +37,13 @@ function App() {
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
         </div>
+      </section>
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+            <CalorieTracker activities={state.activities}/>
+        </div>
+
       </section>
 
       <section className=" p-10 mx-auto max-w-4xl">
