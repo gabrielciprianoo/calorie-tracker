8bb9f8ff4e2519a4f1afcee7777715feeaa8a340
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-21 12:18:10 -0600

    feature: localstorage implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
@@ -6,6 +6,9 @@ import ActivityList from "./components/ActivityList";
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
 
+  useEffect(()=>{
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  },[state.activities])
   return (
     <>
       <header id="register" className="bg-lime-600 py-3">
@@ -18,12 +21,15 @@ function App() {
 
       <section className="bg-lime-500 py-8 md:py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form dispatch={dispatch} />
+          <Form dispatch={dispatch} state={state}/>
         </div>
       </section>
 
       <section className=" p-10 mx-auto max-w-4xl">
-        <ActivityList activities={state.activities} />
+        <ActivityList
+         activities={state.activities}
+         dispatch={dispatch}
+        />
       </section>
     </>
   );
