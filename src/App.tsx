0fd10fef89c65d20d92ebf69e145a18d0ec83c46
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-21 12:48:47 -0600

    feature: button to allows restart app

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import ActivityList from "./components/ActivityList";
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
 
-  useEffect(()=>{
-    localStorage.setItem('activities', JSON.stringify(state.activities))
-  },[state.activities])
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canRestart = () =>state.activities.length;
+
   return (
     <>
       <header id="register" className="bg-lime-600 py-3">
@@ -16,20 +19,27 @@ function App() {
           <h1 className="tex-center px-5 lg:px-0 text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+
+          <button
+            className="uppercase bg-gray-800 p-3 rounded-lg text-white font-bold text-sm cursor-pointer disabled:opacity-20 disabled:cursor-not-allowed"
+            onClick={() => {
+              dispatch({ type: "reset-app" });
+            }}
+            disabled={!canRestart()}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
 
       <section className="bg-lime-500 py-8 md:py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form dispatch={dispatch} state={state}/>
+          <Form dispatch={dispatch} state={state} />
         </div>
       </section>
 
       <section className=" p-10 mx-auto max-w-4xl">
-        <ActivityList
-         activities={state.activities}
-         dispatch={dispatch}
-        />
+        <ActivityList activities={state.activities} dispatch={dispatch} />
       </section>
     </>
   );
