ab6b6b79c8dd59c958634c3e519c806e00b3e66f
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-14 10:53:50 -0600

    migration: calorie tracker to context API for manage state without props

--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,27 +1,65 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, Dispatch, ReactNode, useReducer } from "react";
-import { ActivityActions, activityReducer, ActivityState, initialState } from "../reducers/activity-reducer";
+import { createContext, Dispatch, ReactNode, useReducer, useMemo } from "react";
+import {
+  ActivityActions,
+  activityReducer,
+  ActivityState,
+  initialState,
+} from "../reducers/activity-reducer";
 
 type ActivityProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 type ActivityContextProps = {
-    state : ActivityState,
-    dispatch: Dispatch<ActivityActions>
-}
+  state: ActivityState;
+  dispatch: Dispatch<ActivityActions>;
+  caloriesConsumed: number;
+  caloriesBurned: number;
+  netCalories: number;
+};
 
-export const ActivityContext = createContext<ActivityContextProps>(null!)
+export const ActivityContext = createContext<ActivityContextProps>(null!);
 
 export const ActivityProvider = ({ children }: ActivityProviderProps) => {
-    const [state, dispatch] = useReducer(activityReducer, initialState)
-    
-    return (
-        <ActivityContext.Provider value={{
-            state,
-            dispatch
-        }}>
-            {children}
-        </ActivityContext.Provider>
-    )
-}
+  const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  const caloriesConsumed = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + +activity.calories : total,
+        0
+      ),
+    [state.activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + +activity.calories : total,
+        0
+      ),
+    [state.activities]
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [caloriesBurned, caloriesConsumed]
+  );
+
+  return (
+    <ActivityContext.Provider
+      value={{
+        state,
+        dispatch,
+        caloriesConsumed,
+        caloriesBurned,
+        netCalories,
+      }}
+    >
+      {children}
+    </ActivityContext.Provider>
+  );
+};
